afc1c5238d4caf55000dbe107315a51c89a12b81
m1rby <seniafur2000@gmail.com>
2024-04-15 17:41:02 -0100

    Second commit

--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@ public:
         cout << "Модель: " << model << endl;
         cout << "Вес: " << weight << " кг" << endl;
         cout << "Максимальная скорость: " << maxSpeed << " м/с" << endl;
-        cout << "Максимальный заряд батареи: " << batteryLife << " процентов" << endl;
+        cout << "Максимальный заряд батареи: " << batteryLife << "процентов" << endl;
         cout << "Заряд батареи: " << currentBattery << " процентов" << endl;
     }
 
@@ -37,6 +37,56 @@ public:
     }
 
     virtual ~Drone() {} // Виртуальный деструктор
+};
+
+class ScoutDrone : public Drone {
+private:
+    int cameraResolution;
+
+public:
+    ScoutDrone(string model, int weight, float maxSpeed, int batteryLife, int cameraResolution)
+        : Drone(model, weight, maxSpeed, batteryLife), cameraResolution(cameraResolution) {
+        currentBattery = 20;
+        }
+
+    void printInfo() override {
+        Drone::printInfo();
+        cout << "Разрешение камеры: " << cameraResolution << " МП" << endl;
+    }
+
+    void fly() override {
+        if (currentBattery >= 25) {
+            cout << "Разведывательный дрон начал разведку местности." << endl;
+            currentBattery -= 20;
+        } else {
+            cout << "Разведывательный дрон не может взлететь. Заряда слишком мало. ОПАСНО!!!" << endl;
+        }
+    }
+};
+
+class CargoDrone : public Drone {
+private:
+    float maxCargoWeight;
+
+public:
+    CargoDrone(string model, int weight, float maxSpeed, int batteryLife, float maxCargoWeight)
+        : Drone(model, weight, maxSpeed,  batteryLife), maxCargoWeight(maxCargoWeight) {
+        currentBattery = 35; // Установим начальный заряд
+    }
+
+    void printInfo() override {
+        Drone::printInfo();
+        cout << "Максимальный вес груза: " << maxCargoWeight << " кг" << endl;
+    }
+
+    void fly() override {
+        if (currentBattery >= 20) {
+            cout << "Грузовой дрон выполняет доставку груза." << endl;
+            currentBattery -= 20;
+        } else {
+            cout << "Грузовой дрон не может взлететь. Заряд меньше 20 процентов. ОПАСНО" << endl;
+        }
+    }
 };
 
 int main() {
