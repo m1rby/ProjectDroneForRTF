df6b9af08589e7e29d4fbd2d3559722d1465d593
m1rby <seniafur2000@gmail.com>
2024-04-15 18:10:43 -0100

    Third commit

--- main.cpp
+++ main.cpp
@@ -59,7 +59,7 @@ public:
             cout << "Разведывательный дрон начал разведку местности." << endl;
             currentBattery -= 20;
         } else {
-            cout << "Разведывательный дрон не может взлететь. Заряда слишком мало. ОПАСНО!!!" << endl;
+            cout << "Разведывательный дрон не может взлететь. Время полета меньше 25 минут. ОПАСНО!!!" << endl;
         }
     }
 };
@@ -90,5 +90,59 @@ public:
 };
 
 int main() {
+    setlocale(LC_ALL,"");
+    int choice;
+
+    do {
+        cout << "Выберите дрон:" << endl;
+        cout << "1. Разведывательный дрон" << endl;
+        cout << "2. Грузовой дрон" << endl;
+        cout << "0. Выйти из программы" << endl;
+        cin >> choice;
+
+        if (choice == 0) {
+            break;
+        }
+
+        Drone *selectedDrone;
+        if (choice == 1) {
+            selectedDrone = new ScoutDrone("Scout X1", 2, 15.5, 100, 100);
+        } else if (choice == 2) {
+            selectedDrone = new CargoDrone("Cargo Master 5000", 5, 10.0, 100, 100);
+        } else {
+            cout << "Неверный выбор." << endl;
+            continue;
+        }
+
+        selectedDrone->printInfo();
+
+        do {
+            cout << "Выберите действие:" << endl;
+            cout << "1. Проверить заряд батареи" << endl;
+            cout << "2. Зарядить батарею" << endl;
+            cout << "3. Взлететь" << endl;
+            cout << "0. Выбрать другой дрон" << endl;
+            cin >> choice;
+
+            switch (choice) {
+                case 1:
+                    selectedDrone->printInfo();
+                    break;
+                case 2:
+                    selectedDrone->chargeBattery();
+                    break;
+                case 3:
+                    selectedDrone->fly();
+                    break;
+                case 0:
+                    break;
+                default:
+                    cout << "Неверный выбор." << endl;
+            }
+        } while (choice != 0);
+
+        delete selectedDrone;
+    } while (true);
+
     return 0;
 }
